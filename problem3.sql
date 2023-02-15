f2ae525a725e20fe768cc45453c0aa6df6929810
JameshaRicketts <jamesharicketts@gmail.com>
2023-02-15 01:18:01 +0000

    done

---
+++ problem3.sql
@@ -0,0 +1,4 @@
+SELECT * FROM employee WHERE NOT (first_name = 'Steve') AND salary > 50000;
+--#### Problem 3: Write a statement that will query the above
+--table for all employees who earn more than $50,000 and are
+--NOT named 'Steve'
